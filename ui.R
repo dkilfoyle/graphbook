27b1e9fbd86da57e859009c24ec695b1ab7d2980
dkilfoyle <dkilfoyle@xtra.co.nz>
2014-07-12 09:55:37 +1200

    Use selectize plugins remove_button and drag_drop for multiple=T selectinputs Add placeholder for scatterplot

--- ui.R
+++ ui.R
@@ -8,12 +8,16 @@ data(iris)
 data(diamonds)
 
 shinyUI(fluidPage(
+  
+  includeScript("www/jquery-ui-1.10.3.custom.min.js"),
 
   # Application title
   titlePanel("Graph Cookbook"),
 
   navlistPanel(
     "Graphs",
+    
+    # boxplot tabpanel
     tabPanel("Box Plot", value="boxplot",
       tabsetPanel(
         tabPanel("Plot", plotOutput("myPlot")),
@@ -38,6 +42,37 @@ shinyUI(fluidPage(
                h4("col3")
         ) 
       )
-    ), id = "navlist", fluid=F
+    ), # end of boxplot panel
+    
+    # boxplot tabpanel
+    tabPanel("Scatter Plot", value="scatterplot",
+       tabsetPanel(
+         tabPanel("Plot", plotOutput("myPlot")),
+         tabPanel("Code", aceEditor("myCode","", mode="r"))
+       ),
+       hr(),
+       fluidRow(
+         column(3,
+                h4("Select Fields"),
+                selectInput("myDataFrame", "Dataframe", choices=getDataFrames()),
+                selectizeInput("myNumeric", "Numeric", choices=getdfinfo(getDataFrames()[1])$numerics$name, 
+                               options=list(dropdownParent="body", plugins=list(remove_button="", drag_drop="")), multiple=T)
+         ),
+         column(4, offset = 1,
+                h4("Labels"),
+                textInput("myTitle", "title"),
+                textInput("myXLab", "X Axis"),
+                textInput("myYLab", "Y Axis")
+         ),
+         column(4,
+                h4("col3")
+         ) 
+       )
+    ), # end of boxplot panel    
+    
+    
+    
+    
+    id = "navlist", fluid=F
   )
 ))
